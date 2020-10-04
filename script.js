e8ef52716337ee06135879ccb37a8dfc5be84301
konstapascal <pascalkonstantinos@gmail.com>
2020-10-04 03:45:46 +0200

    small changes to js and css

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const keys = document.querySelector('.calculator-buttons');
+const calculator = document.querySelector;
 const display = document.querySelector('.calculator-display');
+const keys = document.querySelector('.calculator-buttons');
 
 keys.addEventListener('click', event => {
 	const key = event.target;
 	const keyValue = key.textContent;
 	const displayValue = display.textContent;
 
-	if (displayValue === '0') {
-		display.textContent = keyValue;
-	} else {
-		display.textContent += keyValue;
+	// check if key is number
+	if (key.classList.contains('number')) {
+		if (displayValue === '0') {
+			display.textContent = keyValue;
+		} else {
+			display.textContent += keyValue;
+		}
 	}
 
+	// check if key is operator
+	if (key.classList.contains('operator')) {
+		console.log(key);
+	}
+
+	// check if key pressed is clear
 	if (key.classList.contains('clear')) {
 		display.innerHTML = '0';
 	}
